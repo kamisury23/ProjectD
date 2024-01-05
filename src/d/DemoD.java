8d1acbcb4847a40693fe348d00eba3c72009bc1d
Karina <Karina@10.0.0.31>
2024-01-05 00:45:24 -0500

    Practicing git push

--- src/d/DemoD.java
+++ src/d/DemoD.java
@@ -2,18 +2,20 @@ package d;
 
 public class DemoD {
 	public static void main(String[] args) {
-		
+
 		System.out.println("April");
-		
+
 		// coming from eclipse. local repo
 		System.out.println("Wednesday, Thursday...");
-		
-	        System.out.println("No sleep since JAVA");
 
-		//coming from github. remote repo 
-	        System.out.println("No gym since JAVA");
+		System.out.println("No sleep since JAVA");
+
+		// coming from github. remote repo
+		System.out.println("No gym since JAVA");
 		
+		//coming from eclipse. local repo. git push
+		System.out.println("Keep focus, keep grinding...");
+
 	}
 
-	
 }
